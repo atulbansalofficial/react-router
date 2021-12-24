d3d854db2aac2a49e28073200efe6a8e056ca73d
atulbansalofficial <atulbansal9@gmail.com>
2021-12-25 01:36:11 +0530

    service page

---
+++ src/component/pages/service.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class service extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Service Page</h1>
+      </div>
+    );
+  }
+}
